f23fe366f2c2bf8c117c1db05bfe01e7117c897f
haidata <hai0206001@gmail.com>
2024-09-07 22:37:12 +0700

    Update day3_baitap.sql

--- day3_baitap.sql
+++ day3_baitap.sql
@@ -2,4 +2,50 @@
 SELECT NAME FROM CITY
 WHERE Population > 120000
 AND CountryCode = 'USA'
+  
 --baitap 2
+SELECT * FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+--BAITAP 3
+SELECT CITY, STATE
+FROM STATION;
+
+--BAITAP 4
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY LIKE 'a%' 
+OR CITY LIKE 'e%'
+OR CITY LIKE 'i%'
+OR CITY LIKE 'o%'
+OR CITY LIKE 'u%'
+
+---BAITAP 5
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY LIKE '%a'
+OR CITY LIKE '%e'
+OR CITY LIKE '%i'
+OR CITY LIKE '%o'
+OR CITY LIKE '%u'
+
+--BAITAP 6
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY NOT LIKE 'a%'
+AND CITY NOT LIKE 'e%'
+AND CITY NOT LIKE 'i%'
+AND CITY NOT LIKE 'u%'
+AND CITY NOT LIKE 'o%'
+
+--BAITAP 7
+SELECT name FrOM EMPLOYEE
+ORDER BY name ASC
+
+--BAITAP 8
+select name from employee
+where salary >2000 
+and months <10
+order by employee_id 
+
+--BAITAP 9
+select product_id from products
+where low_fats = 'Y'
+and recyclable = 'Y'
