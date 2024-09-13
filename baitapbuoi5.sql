006e388545be1f71c1ffcd9308eb04e2802ccbcd
haidata <hai0206001@gmail.com>
2024-09-13 16:34:26 +0700

    Update baitapbuoi5.sql

--- baitapbuoi5.sql
+++ baitapbuoi5.sql
@@ -3,3 +3,17 @@ select distinct city from station
 where id % 2=0
 
 BAITAP 2
+select Count(city) - count(distinct city)
+from station
+
+ BAITAP 4
+SELECT ROUND(cast(SUM(item_count * order_occurrences) / SUM(order_occurrences) AS DECIMAL), 1) AS mean
+FROM items_per_order;
+
+baitap5
+SELECT CANDIDATE_ID FROM CANDIDATES
+WHERE SKILL in ('Python','Tableau','PostgreSQL')
+group by candidate_id
+HAVING count (skill) =3
+
+BAITAP 5
